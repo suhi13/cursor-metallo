358dece9feddbd549d2e55b90976e4df72676fad
Yuliia Sukhova <yuliia.sukhova@tech-stack.io>
2022-07-25 21:10:20 +0300

    added app entry point

---
+++ src/main/java/HW1/App.java
@@ -0,0 +1,7 @@
+package HW1;
+
+public class App {
+    public static void main(String[] args) {
+        new AppRunner().runApp();
+    }
+}
---
+++ src/main/java/HW1/AppRunner.java
@@ -0,0 +1,6 @@
+package HW1;
+
+public class AppRunner {
+    public static void runApp() {
+    }
+}
